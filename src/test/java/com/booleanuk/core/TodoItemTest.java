7c51a46b0f496d5a2a3b17c918902c59fd76440c
Axel Hansson <axel.co.hansson@gmail.com>
2024-01-18 09:23:01 +0100

    Tests Written

--- src/test/java/com/booleanuk/core/TodoItemTest.java
+++ src/test/java/com/booleanuk/core/TodoItemTest.java
@@ -4,5 +4,26 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class TodoItemTest {
+    @Test
+    public void testGetters(){
+        TodoItem item = new TodoItem("Clean", "done before wednesday", "incomplete");
+
+        Assertions.assertEquals("incomplete", item.getStatus());
+        Assertions.assertEquals("Clean", item.getTitle());
+        Assertions.assertEquals("done before wednesday", item.getDetail());
+    }
+
+    @Test
+    public void testSetters(){
+        TodoItem item = new TodoItem("Cook", "done before saturday", "complete");
+
+        item.setTitle("Clean");
+        item.setDetail("done before wednesday");
+        item.chageStatus();
+
+        Assertions.assertEquals("incomplete", item.getStatus());
+        Assertions.assertEquals("Clean", item.getTitle());
+        Assertions.assertEquals("done before wednesday", item.getDetail());
+    }
 
 }
