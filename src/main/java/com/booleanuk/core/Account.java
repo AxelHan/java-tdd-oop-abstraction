03ae4e7aedcbbbbe4499ef4025d61049f3529210
Axel Hansson <axel.co.hansson@gmail.com>
2024-01-18 10:53:46 +0100

    Code written to pass

--- src/main/java/com/booleanuk/core/Account.java
+++ src/main/java/com/booleanuk/core/Account.java
@@ -3,15 +3,20 @@ package com.booleanuk.core;
 public class Account {
 
     private boolean isEnabled;
+    private String email;
+    private String password;
 
-    public Account(){
-
+    public Account(String email, String password){
+        this.isEnabled = false ;
+        this.email = email;
+        this.password = password;
     }
 
     public void changeStatus(){
+        this.isEnabled = !this.isEnabled;
     }
 
     public boolean getStatus(){
-        return false;
+        return this.isEnabled;
     }
 }
--- src/main/java/com/booleanuk/core/User.java
+++ src/main/java/com/booleanuk/core/User.java
@@ -5,15 +5,36 @@ public class User {
     private String name;
     private Account account;
 
-    public User(String name ){
+    public User(String name){
         this.name = name;
+        this.account = null;
+
     }
 
     public String createAccount(String email, String password){
-        return "";
+        if(!email.contains("@")){
+            return "invalid email";
+        }
+        if (password.length() < 8){
+            return "invalid password";
+        }
+        this.account = new Account(email, password);
+        return "Account created";
     }
 
     public String login(String email, String password){
-        return "";
+        if(this.account == null){
+            return "You have not created an account";
+        }
+        if (!this.account.getStatus()){
+            return "Account disabled";
+        }
+        else {
+            return "Account enabled";
+        }
+    }
+
+    public Account getAccount() {
+        return account;
     }
 }
